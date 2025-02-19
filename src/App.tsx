2604980ebcaeaeaf11e1597eefd8f7fd5d361f1d
KEVIN-PC\Kevin <kevin.walsh@flypaperplane.com>
2025-02-18 21:01:41 -0600

    update API endpoint in Calculator component and add proxy to package.json

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const Calculator = () => {
     };
 
     try {
-      const response = await fetch('http://ec2-3-95-155-9.compute-1.amazonaws.com/api/calculator', {
+      const response = await fetch('/api/calculator', {
         referrerPolicy: 'unsafe-url',
         method: 'POST',
         headers: {
