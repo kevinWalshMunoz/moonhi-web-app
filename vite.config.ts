cf1ee0819a1dc36a30f4e8f15b377405c1993965
KEVIN-PC\Kevin <kevin.walsh@flypaperplane.com>
2025-02-18 21:11:13 -0600

    add proxy configuration for API in vite.config.ts and remove proxy from package.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://ec2-3-95-155-9.compute-1.amazonaws.com", // Cambia esto por tu API
+        changeOrigin: true,
+        secure: false, // Si el backend usa HTTPS con certificado autofirmado, usa false
+      },
+    },
+  },
 })
